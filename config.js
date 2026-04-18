6358a1595594134f89dd68fd9db72699ef1ec757
ogifamily091 <ogifamily.091@gmail.com>
2026-04-19 00:22:49 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,6 @@
 window.OGI_APP_CONFIG = {
-  apiBaseUrl: 'https://script.google.com/macros/s/AKfycbwtlGXMATvF06y61X5j67IiRfJUfoXBe8Lc6eXLnNTHAPjWH3Jjmq5uj5TakPXMyWFY/exec'
+  apiBaseUrl: "https://admin.ogifamily.my.id",
+  // optional:
+  // rpcUrl: "https://admin.ogifamily.my.id/api/rpc",
+  credentials: "omit"
 };
